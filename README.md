01abf00277b615db981afb0857d1d3098f33c77f
bergren2 <dan@bergren.dev>
2019-09-27 22:07:40 -0500

    build out Hello World capabilities

--- README.md
+++ README.md
@@ -3,6 +3,14 @@ Jira CLI tool written in Rust
 
 ![Anguirus vs. Godzilla](anguirus.jpg)
 
+## Development
+
+Run the following to simulate the CLI tool.
+
+    $ cargo run
+
+You can pass in `-q` if you want to supress messaging so it behaves like it would when finally compiled.
+
 ## Useful Links
 
 - [Creating API Tokens](https://id.atlassian.com/manage/api-tokens)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use human_panic::setup_panic;
+use serde_json::json;
+use structopt::StructOpt;
+
+
+// Search for a pattern in a file and display the lines that contain it.
+#[derive(StructOpt)]
+struct Cli {
+    // The pattern to look for
+    pattern: String,
+    // The path to the file to read
+    #[structopt(parse(from_os_str))]
+    path: std::path::PathBuf,
+    // Output JSON instead of human readable messages
+    #[structopt(long = "json")]
+    json: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    setup_panic!();
+
+    let args = Cli::from_args();
+
+    if args.json {
+        println!("{}", json!({
+            "type": "message",
+            "content": "Hello world",
+        }));
+    } else {
+        println!("Hello world");
+    }
 }
